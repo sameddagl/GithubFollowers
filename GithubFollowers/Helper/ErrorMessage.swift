df5dc16b3f377a901247e1a7c425f407a825dd9e
sameddagl <samedddagl@gmail.com>
2022-11-23 12:14:49 +0300

    User info screen build v1

---
+++ GithubFollowers/Helper/ErrorMessage.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  GithubFollowers
+//
+//  Created by Samed Dağlı on 23.11.2022.
+//
+
+import Foundation
---
+++ GithubFollowers/Views/ViewControllers/UserInfo/GFFollowersItemVC.swift
@@ -0,0 +1,29 @@
+//
+//  GFFollowersItemVC.swift
+//  GithubFollowers
+//
+//  Created by Samed Dağlı on 23.11.2022.
+//
+
+import UIKit
+
+class GFFollowersItemVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
